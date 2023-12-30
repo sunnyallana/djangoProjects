cf9d4196fd2156880e40ba4c48d6c9724fca4c8f
sunnyallana <study.sunnyallana@gmail.com>
2023-12-31 01:17:47 +0500

    Models

--- eduSphere/eduSync/admin.py
+++ eduSphere/eduSync/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class userModel(UserAdmin):
+    list_display = ['username', 'userType']
+
+admin.site.register(customUser, userModel)
